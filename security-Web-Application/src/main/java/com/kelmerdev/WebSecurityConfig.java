a24743dea588f2f0d05d9f7917567ef49ef0abf3
Kelmer-hd <50677633+Kelmer-hd@users.noreply.github.com>
2025-01-07 16:07:38 -0500

    Aplicación usando spring security basic

---
+++ security-Web-Application/src/main/java/com/kelmerdev/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.kelmerdev;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("USER", "ADMIN")
+                .build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("test")
+                .password("test123")
+                .roles("USER")
+                .build();
+
+        UserDetails manager = User.withDefaultPasswordEncoder()
+                .username("manager")
+                .password("manager123")
+                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin, user2, manager);
+    }
+}
